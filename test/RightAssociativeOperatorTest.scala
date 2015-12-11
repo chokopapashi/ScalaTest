2972c6601bf65aeb369f7c09164f0275fd8bd415
chokopapashi <chokopapashi@gmail.com>
2015-12-11 23:29:02 +0900

    commit changes

--- test/RightAssociativeOperatorTest.scala
+++ test/RightAssociativeOperatorTest.scala
@@ -1,7 +1,8 @@
 
 object FibonacciNumber {
 //    val fibs: Stream[BigInt] = BigInt(0) #:: BigInt(1) #:: fibs.zip(fibs.tail).map { n => n._1 + n._2 }
-    val xs: Stream[Int] = xs.#::(1)
+//    val xs: Stream[Int] = xs.#::(1)
+    val xs: Stream[Int] = 1 #::.xs
 }
 
 
