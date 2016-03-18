220a9238e1e9fa2de9c3cc8ef255173f136178c3
chokopapashi <chokopapashi@gmail.com>
2016-03-18 22:42:07 +0900

    commit changes.:w

--- test/polymorphism_test_2.scala
+++ test/polymorphism_test_2.scala
@@ -1,10 +1,9 @@
-
-
+:reset 
 
 import scala.reflect.ClassTag
 import scala.reflect.runtime.{universe => ru}
 
-object test {
+object Test {
 
 trait TA { val s: String }
 class CA(val s: String) extends TA
@@ -12,7 +11,8 @@ class CB(val s: String) extends TA
 
 type TASub = (CA with CB)
 
-def newInstance[A >: TASub <: TA](s: String)(implicit tA: ru.TypeTag[A]): A = {
+//def newInstance[A >: TASub <: TA : ru.TypeTag](s: String): A = {
+def newInstance[A : ru.TypeTag](s: String): A = {
     /* Instantiating a Type at Runtime for 2.10 */
     val m = ru.runtimeMirror(getClass.getClassLoader)               /* Obtaining a mirror */
     val cs = ru.typeOf[A].typeSymbol.asClass                        /* Obtaining a class */
@@ -22,22 +22,31 @@ def newInstance[A >: TASub <: TA](s: String)(implicit tA: ru.TypeTag[A]): A = {
     ctorm(s).asInstanceOf[A]
 }
 
-//def parseParam[A >: TASub <: TA](s: String)(implicit tA: ru.TypeTag[A]): A
-def parseParam[A >: TASub <: TA : ru.TypeTag](s: String): A = {
-    newInstance[A](s)
-}
-
-def func1(x: Int) {
+def parseParam[A >: TASub <: TA : ru.TypeTag](x: Int, s: String): A
+//def parseParam[A : ru.TypeTag](x: Int, s: String): A
+= {
     x match {
-        case 1 =>
-        case 2 =>
+        case 1 => newInstance[CA](s).asInstanceOf[A]
+        case 2 => newInstance[CB](s).asInstanceOf[A]
     }
 }
 
+def func2(x: CA)  = "This is CA"
+def func2(x: CB)  = "This is CB"
+
+//def func1(x: Int) {
+//    val c = x match {
+//        case 1 => parseParam[CA]("a").asInstanceOf[CA]
+//        case 2 => parseParam[CB]("B").asInstanceOf[CB]
+//    }
+////    func2(c)
+//}
+
 }
 
-import test._
+import Test._
 
-parseParam[CA]("a")
-parseParam[CB]("b")
+//func1(1)
+
+func2(parseParam[TASub](1, "a"))
 
